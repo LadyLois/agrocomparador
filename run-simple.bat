fe9f54ce5d4790fec57225592a56bb1508d4713a
Myself <134278159+LadyLois@users.noreply.github.com>
2026-04-21 18:34:25 +0200

    Delete run-simple.bat

--- run-simple.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set DB_HOST=localhost
-set DB_USER=admin
-set DB_PASSWORD=AgroComparador2026!
-set DB_NAME=comparador
-set PORT=8080
-java -cp ".;mysql-connector-java-9.0.0.jar" agrocomparador
-pause
